6aebf383b928ac98326e592ecb22b93754f0c8d8
technotidetether <technotidetether@gmail.com>
2025-11-17 13:04:57 +0530

    initial commit
    Signed-off-by: technotidetether <technotidetether@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+![Alt text](RegistrationFormGUI.png "Registration Form GUI")
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Registration registration = new Registration();
+        registration.setVisible(true);
+    }
+}
+
